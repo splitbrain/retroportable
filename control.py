dde0a8b66ba91bfeab0941fbffe3005ed64bdf6f
Andreas Gohr <andi@splitbrain.org>
2019-03-31 13:29:01 +0000

    some cleanup in the python formatting

--- control.py
+++ control.py
@@ -23,6 +23,7 @@ high = 75
 volumes = [100, 80, 45, 0]
 volume = 0
 
+
 # callback for the volume button
 def cycle_volume(channel):
     global volume
@@ -30,11 +31,11 @@ def cycle_volume(channel):
     if volume >= len(volumes):
         volume = 0
     print("volume %d" % volumes[volume])
-    subprocess.call(['/usr/bin/amixer','sset', 'PCM', str(volumes[volume])+'%'])
+    subprocess.call(['/usr/bin/amixer', 'sset', 'PCM', str(volumes[volume])+'%'])
     subprocess.call(['/usr/bin/aplay', sys.path[0] + '/beep.wav'])
 
 # initialize with full volume
-subprocess.call(['/usr/bin/amixer','sset', 'PCM', str(volumes[volume])+'%'])
+subprocess.call(['/usr/bin/amixer', 'sset', 'PCM', str(volumes[volume])+'%'])
 
 # setup GPIO
 GPIO.setmode(GPIO.BCM)
@@ -52,15 +53,16 @@ try:
         tfile.seek(0)
         temp = float(tfile.read()) / 1000
         print temp
- 
+
         GPIO.output(pin_g, temp > low)
         GPIO.output(pin_y, temp > mid)
         GPIO.output(pin_r, temp > high)
         GPIO.output(pin_fan, temp > mid)
- 
-        sleep(10);
+
+        sleep(10)
 except:
     print "exiting"
 finally:
     tfile.close()
     GPIO.cleanup()
+
